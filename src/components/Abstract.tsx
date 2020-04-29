83edca9abc0a72328110fdbb2e1c8b75ac8101f3
aso <titechei@gmail.com>
2020-04-29 10:34:18 +0900

    update contents: repository, wish

--- src/components/Abstract.tsx
+++ src/components/Abstract.tsx
@@ -6,6 +6,11 @@ const Title = styled.h1({
   fontWeight: 'bolder',
   padding: '1.5em 0 1em'
 });
+const Header = styled.h2({
+  fontSize: '1.4rem',
+  margin: '1em 0',
+  fontWeight: 'bolder'
+});
 
 export const Abstract: React.FC = () => (
   <>
@@ -18,6 +23,10 @@ export const Abstract: React.FC = () => (
     </p>
     <p>
       値は推定値です。感染者数のデータが少ない都道府県ほど推定誤差が大きくなる傾向にあります。
+    </p>
+    <Header>お願い</Header>
+    <p>
+      当サイトの値を元に、自身の外出活動を再開する目安にしないでください。1.0を下回っていることは収束していることを意味しません。(あくまで収束へと向かっているだけであり、この状態をしばらく維持する必要があります。)
     </p>
   </>
 );
